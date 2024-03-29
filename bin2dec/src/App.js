f28cac6788528e0c512102da567adb7c2a8f5bd8
minjamoddev <2greendragons@gmail.com>
2024-03-28 18:57:00 -0700

    initial project setup

---
+++ bin2dec/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <label>
+        Binary Input: <input name="binInput"/>
+      </label>
+    </div>
+  );
+}
+
+export default App;
